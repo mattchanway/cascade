2c4bdf43809351a2ce30a611116e84fe22318285
mchanway <mchanway@LAPTOP-6MOKM2O6.localdomain>
2023-03-05 11:10:49 -0800

    timecard check feature incomplete

--- src/components/MyTimecards.js
+++ src/components/MyTimecards.js
@@ -3,6 +3,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import UserContext from './UserContext';
 import baseURL from '../helpers/constants';
+import { Table } from 'react-bootstrap';
 
 
 
@@ -10,7 +11,8 @@ function MyTimecards() {
 
     const { employeeId, position, userNotFound, firstName } = useContext(UserContext);
     const [serverError, setServerError] = useState(false);
-    const [timecardsData, setTimecardsData] = useState([]);
+    const [timecardTable, setTimecardTable] = useState([]);
+    // const [timecardsData, setTimecardsData] = useState([]);
     const WEEKDAYS = {0: 'Monday', 1:'Tuesday', 2:'Wednesday',3:'Thursday',4: 'Friday'}
     
 
@@ -19,8 +21,9 @@ function MyTimecards() {
         async function getIndividualTimecards() {
             try {
             let res = await axios.get(`${baseURL}/timecards/reports/weekly/${employeeId}`);
-            console.log(res)
-            generateTable(res.data)
+                console.log('resdata', [...res.data])
+            // let table = generateTable(res.data)
+            // setTimecardTable(table);
             }
             catch (e) {
                 setServerError(true)
@@ -39,25 +42,26 @@ function MyTimecards() {
         let firstRow = new Array(today+1).fill(false);
         while(today >= 0){
             let curr = table.pop();
-            firstRow[today] = curr === null ? false : true;
+            firstRow[today] = curr === null ? null : curr;
             today--;
         }
         let secondRow = new Array(7).fill(false);
         let counter = 6;
         while(counter >= 0){
             let curr = table.pop();
-            secondRow[counter] = curr === null ? false : true;
+            secondRow[counter] = curr === null ? null : curr;
             counter--;
         }
         counter = 6;
         let lastRow = new Array(7).fill(false);
         while(counter >= 0){
             let curr = table.pop();
-            lastRow[counter] = curr === null ? false : true;
+            lastRow[counter] = curr === null ? null : curr;
             counter--;
         }
-        res = [lastRow, secondRow, firstRow]
-        console.log(res)
+       res= [lastRow, secondRow, firstRow]
+        return res
+       
     }
 
 
@@ -66,10 +70,43 @@ function MyTimecards() {
     if (employeeId === null && userNotFound === true) {
         return <Navigate to="/login" replace={true}></Navigate>
     }
-
+ 
     return (
         <div>
             <h2>{firstName}, these are the timecards you've been submitted over the past 2 weeks.</h2>
+            {/* {timecardTable && <Table>
+                <thead>
+                    <tr>
+                    <th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                    <th>Saturday</th>
+                    <th>Sunday</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                {timecardTable[0] && timecardTable[0].map(cell=> <td>{cell&&cell.job_id}</td>)
+
+                }
+                </tr>
+                <tr>
+                {timecardTable[1] && timecardTable[1].map(cell=> <td>{cell&&cell.job_id}</td>)
+
+                }
+                </tr>
+                <tr>
+                {timecardTable[2] && timecardTable[2].map(cell=> <td>{cell&&cell.job_id}</td>)
+
+                }
+                </tr>
+                    </tbody>
+                
+                
+                
+                </Table>} */}
             
 
         </div>
