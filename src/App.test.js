373d47a94040bd548cfbfb03210ba25370829475
mchanway <mchanway@LAPTOP-6MOKM2O6.localdomain>
2023-03-12 15:25:59 -0700

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, fireEvent, screen, act } from "@testing-library/react";
 import App from './App';
 import axios from 'axios'
 
 jest.mock('axios')
 
-it('Shows the login box when rendered', () =>{
+it('Shows the login box when rendered and no user is found', async () => {
 
-    render(<App></App>)
-    axios.get.mockResolvedValue({
-        data:{
-            noUser: 'Cannot find'
-        }
-    })
-   
+    const mockNoUser = {data: {
+        noUser: 'Cannot find'
+    }}
+    axios.get = jest.fn().mockResolvedValue(mockNoUser)
+    await act(async () => {
+        
+        render(<App></App>)
+      });
     expect(screen.getByText('Login')).toBeInTheDocument()
 
+})
 
+it('Shows the jobs list when rendered and valid user is found', async () => {
+
+    const mockJobsList = []
+
+    const mockUser = {data: {
+        employeeId: res.data.employee_id,
+            position: res.data.position,
+            firstName: res.data.first_name,
+            lastName: res.data.last_name,
+            userNotFound: false,
+            firstLogin: res.data.first_login
+    }}
+    axios.get = jest.fn().mockResolvedValue(mockUser)
+    await act(async () => {
+        
+        render(<App></App>)
+      });
+    expect(screen.getByText('Login')).toBeInTheDocument()
 
 })
